52d3910f305f2701fd3b115ff6118c5799224353
saleh sepasi <saleh.sepasi.ds@gmail.com>
2026-09-09 20:32:55 +0330

    add title

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Fun Resume Generator",
+    page_icon="📄",
+    layout="wide"
+)
+
+st.title("Fun Resume Generator")
+st.write("Turn your boring resume into something unforgettable! 😎")
+st.header("Tell us about yourself")
